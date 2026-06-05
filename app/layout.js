db20b998f1af4fe413d93946b6eac2c42a230fce
Maksym kravchuk <seokont@gmail.com>
2026-06-05 18:17:37 +0300

    hh

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "../style.css";
+import Script from "next/script";
 
 export const metadata = {
   metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000"),
@@ -46,7 +47,24 @@ export const viewport = {
 export default function RootLayout({ children }) {
   return (
     <html lang="uk">
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="beforeInteractive">{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NTD72C5Z');`}</Script>
+      </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NTD72C5Z"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         {children}
         <script
           src="https://aisw.online/widget.js"
